cd3b945bede881f27515d363184d2ec3fb6be8dd
万成 <25022549@qq.com>
2019-07-20 22:08:11 +0800

    add files

--- README.md
+++ README.md
@@ -1,1 +1,195 @@
-# jquery-lazyimg
+# Lazy Loader for html elements
+
+lazyimg 是一个基于jQuery的懒加载图片组件。
+
+ps:目前支持 `全局方法调用` 和 `jquery插件调用` 两种模式
+
+## Getting started
+
+首先确保先引入jquery，之后再引入lazyimg.js，它会产生一个名为`lazyimg`的全局对象，请避免重名。
+
+生成环境地址：
+
+
+### 同时支持 AMD, CommonJS, ES6 调用方式
+
+
+CommonJS
+``` javascript
+var lazyimg = require('lazyimg');
+
+//
+lazyimg.config({
+
+})
+
+//
+lazyimg.listen();
+
+//
+lazyimg.detch();
+
+```
+
+AMD 方式
+``` javascript
+require(['lazyimg'], function(lazyimg) {
+　//
+ lazyimg.config({
+
+ })
+
+ //
+ lazyimg.listen();
+
+ //
+ lazyimg.detch();
+});
+
+```
+
+
+ES6
+
+``` javascript
+import $ from 'jQuery';
+import lazyimg from 'jquery-lazyimg';
+
+$('img').lazyimg();
+// 或者
+lazyimg.listen();
+
+```
+
+
+
+### 加载前景图
+```html
+<img lazy-src="temp/1.jpg" alt="" />
+```
+lazyimg，所以如果只需要懒加载图片，可以不用传入参数
+```js
+lazyimg.listen();
+```
+相当于
+```js
+lazyimg.listen('img[lazy-src]', 'img');
+```
+如果需要在图片触发加载条件后执行回调，可将回调函数作为第三个参数传入
+```js
+lazyimg.listen('img[lazy-src]', 'img', function(obj) {
+	// obj 是当前触发加载条件的jQuery对象
+});
+```
+
+### 加载前景图 (jQuery 插件版本)
+```html
+<img lazy-src="temp/1.jpg" alt="" />
+```
+lazyimg，所以如果只需要懒加载图片，可以不用传入参数
+```js
+$('img').listen();
+```
+
+如果需要在图片触发加载条件后执行回调，可将回调函数作为第三个参数传入
+```js
+$('img').listen(function(obj) {
+	// obj 是当前触发加载条件的jQuery对象
+});
+```
+
+
+### 加载背景图
+```html
+<div class="bg" lazy-bg="temp/bg.jpg"></div>
+```
+
+```js
+lazyimg.listen('.bg', 'bg', function(obj) {
+	// 回调是可选的
+});
+```
+
+### 加载背景图 (jQuery 插件版本)
+```html
+<div class="bg" lazy-bg="temp/bg.jpg"></div>
+```
+
+```js
+$('.bg').listen('bg', function(obj) {
+	// 回调是可选的
+});
+```
+
+### Loading样式
+lazyimg会给每个被监听的img元素加上一个class：`lazy-loading`，可以在项目的css中设置loading效果。
+
+
+## Configuration
+目前提供5个配置项，通过`config`方法设置,要在listen方法前调用。
+```js
+lazyimg.config({
+    timeout: 10, // 每次滚动事件执行延迟
+    buffer: 100, // 屏幕上下方缓冲区域高度
+    loadingClass: 'lazy-loading',
+    srcValue: 'lazy-src',
+    bgValue: 'lazy-bg'
+});
+```
+
+
+## Configuration(jQuery 插件版本)
+目前提供5个配置项，只需要在第一个参数，配置就可以了。
+```js
+$('img').lazyimg({
+    timeout: 10, // 每次滚动事件执行延迟
+    buffer: 100, // 屏幕上下方缓冲区域高度
+    loadingClass: 'lazy-loading',
+    srcValue: 'lazy-src',
+    bgValue: 'lazy-bg'
+},'img',function(obj){
+
+});
+```
+
+
+
+## Method
+
+### detect
+
+某些需要在特定事件中加载的内容，可以在各自的事件处理逻辑后调用此方法，以展示原本被隐藏的内容。
+```js
+lazyimg.detect();
+```
+
+
+### clear
+
+清除对某个或全部对象的监听
+```js
+lazyimg.clear([DOM Object]);
+```
+
+
+
+
+## Method(jQuery 插件版本)
+
+### detect
+
+某些需要在特定事件中加载的内容，可以在各自的事件处理逻辑后调用此方法，以展示原本被隐藏的内容。
+```js
+$('img').lazyimg('detect');
+```
+
+
+### clear
+
+清除对某个或全部对象的监听
+```js
+$('img').clear([DOM Object]);
+```
+
+
+
---
+++ lazyimg.js
@@ -0,0 +1,339 @@
+/**
+ * date: 2017/6/26
+ * author:
+ * desc:
+ */
+
+
+
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // Node, CommonJS之类的
+        module.exports = factory(require('jquery'));
+    } else {
+        // 浏览器全局变量(root 即 window)
+        root.lazyimg = factory(root.jQuery);
+    }
+}(this, function ($) {
+
+    'use strict';
+    // 依赖 window
+    var $win = $(window),
+
+        // 滚动监听是否开启
+        isListening = false,
+
+        // 被监听的元素集合
+        listeners = [],
+        groups = {},
+
+        // 回调函数集合
+        callbacks = [],
+
+        // 默认配置
+        config = {
+            timeout: 10,
+            buffer: 100,
+            loadingClass: 'lazy-loading',
+            srcValue: 'lazy-src',
+            bgValue: 'lazy-bg'
+        },
+
+        that, timer;
+
+
+    /**
+     * 全局主对象
+     * @module lazyimg
+     */
+    var lazyimg = {
+
+        version: '1.1.1',
+
+        /**
+         * 初始化框架
+         * @method _init
+         */
+        _init: function () {
+            that = this;
+        },
+
+        /**
+         * 添加监听元素
+         * @method listen
+         * @param {String|Object} selector 用于选中元素的jQuery选择器,或直接传入一组jQuery对象
+         * @param {String} type 元素类型，可用值 'img'|'bg'|dom'|'fn'|'bat'
+         * @param {Function} callback 加载后的回调函数 参数1:当前触发加载条件的jquery对象
+         */
+        listen: function (selector, type, callback) {
+            var elements = selector,
+                eType = (type || 'img').toLowerCase(),
+                cbIndex;
+
+            // 判断传入的是否选择器
+            if ((typeof selector).toLowerCase() !== 'object') {
+                elements = $(selector || 'img[' + config.srcValue + ']');
+            }
+
+            // 注册回调函数
+            if (callback && typeof callback === 'function') {
+                cbIndex = callbacks.push(callback) - 1;
+            }
+            // 添加批处理组
+            if (eType === 'bat') {
+                groups[cbIndex] = {
+                    objs: [],
+                    cbIndex: cbIndex
+                };
+            }
+
+            // 添加监听元素
+            elements.each(function () {
+                var o = $(this);
+
+                listeners.push({
+                    type: eType,
+                    obj: o,
+                    cbIndex: cbIndex
+                });
+
+                // 加上loading class
+                if (eType === 'img') {
+                    o.addClass(config.loadingClass);
+                }
+            });
+
+            // 启动窗口滚动事件监听
+            if (!isListening) {
+                that._startListen();
+            }
+
+            that.detect();
+        },
+
+        /**
+         * 绑定窗口监听事件
+         * @method _startListen
+         */
+        _startListen: function () {
+            $win.bind('scroll.lazyelem resize.lazyelem', function () {
+                if (!listeners.length) {
+                    return;
+                }
+
+                clearTimeout(timer);
+
+                timer = setTimeout(function () {
+                    that.detect();
+                }, config.timeout);
+            });
+
+            isListening = true;
+        },
+
+        /**
+         * 检测所有元素是否满足加载条件
+         * @method detect
+         */
+        detect: function () {
+            for (var i = 0; i < listeners.length; i++) {
+                var listener = listeners[i],
+                    obj = listener.obj,
+                    cbIndex = listener.cbIndex;
+
+                // 以下情况直接跳过本次循环： 元素不可见 | 不在屏幕区域内
+                if (that._isHidden(obj) || !that._isTrigger(obj)) {
+                    continue;
+                }
+
+                // 开始加载
+                switch (listener.type) {
+                    case 'fn':
+                        break;
+
+                    case 'img':
+                        var src = obj.attr(config.srcValue);
+                        src && obj.attr('src', src).removeAttr(config.srcValue);
+                        break;
+
+                    case 'bg':
+                        var bg = obj.attr(config.bgValue);
+                        bg && obj.css('background-image', 'url(' + bg + ')').removeAttr(config.bgValue);
+                        break;
+
+                    case 'dom':
+                        var script = obj.children('script');
+                        script.length && script.replaceWith(that._minHtml(script.html()));
+                        break;
+
+                    case 'bat':
+                        groups[cbIndex].objs.push(obj);
+                        break;
+                }
+
+                // 删除该项
+                listeners.splice(i--, 1);
+
+                // 调用回调函数
+                if (listener.type !== 'bat' && cbIndex >= 0) {
+                    callbacks[cbIndex](obj);
+                }
+            }
+
+            // 批处理
+            that._bat();
+
+            // 当所有元素加载完毕，停止监听
+            if (listeners.length === 0) {
+                $win.unbind('scroll.lazyelem resize.lazyelem');
+                isListening = false;
+            }
+        },
+
+        /**
+         * 批量处理懒加载元素
+         * @method _bat
+         */
+        _bat: function () {
+            for (var g in groups) {
+                var objs, cb;
+                if (groups.hasOwnProperty(g)) {
+                    objs = groups[g].objs;
+                    cb = callbacks[groups[g].cbIndex];
+
+                    if (objs.length) {
+                        cb(objs);
+                        groups[g].objs = [];
+                    }
+                }
+            }
+        },
+
+        /**
+         * 判断一个jquery对象是否在屏幕区域之间
+         * @method _isTrigger
+         * @param {jQuery Object} obj 单个jquery对象
+         * @return {Boolean}
+         */
+        _isTrigger: function (obj) {
+            var winHeight = $win.height(),
+                winTop = $win.scrollTop(),
+                oHeight = obj.height(),
+                ot = obj.offset().top;
+
+            return (ot + oHeight) > (winTop - config.buffer) && ot < (winTop + winHeight + config.buffer);
+        },
+
+        /**
+         * 判断一个jquery对象是否可见
+         * @method _isHidden
+         * @param {jQuery Object} obj 单个jquery对象
+         * @return {Boolean}
+         */
+        _isHidden: function (obj) {
+            var elem = obj[0];
+
+            if (obj.css('display') === 'none' || !$.contains(elem.ownerDocument, elem)) {
+                return true;
+            }
+            if (elem.offsetWidth <= 0 && elem.offsetHeight <= 0) {
+                return true;
+            }
+            return false;
+        },
+
+        /**
+         * 最小化一段html文本
+         * @method _minHtml
+         * @param {String} html 一段html代码文本
+         * @return {String} 处理后的html文本
+         */
+        _minHtml: function (html) {
+            var rep = /\n+/g,
+                repone = /<!--.*?-->/ig,
+                reptwo = /\/\*.*?\*\//ig,
+                reptree = /[ ]+</ig;
+
+            html = html.replace(rep, '');
+            html = html.replace(repone, '');
+            html = html.replace(reptwo, '');
+            html = html.replace(reptree, '<');
+
+            return html;
+        },
+
+        /**
+         * 配置参数
+         * @method config
+         * @param {Object} opt 配置项 timeout|buffer|loadingClass|srcValue|bgValue
+         */
+        config: function (opt) {
+            $.extend(config, opt);
+        },
+
+        /**
+         * 清除监听对象
+         * @method clear
+         * @param {Object} elem 指定要清除的DOM对象
+         */
+        clear: function (elem) {
+            if (elem) {
+                for (var i = 0; i < listeners.length; i++) {
+                    if (elem === listeners[i].obj[0]) {
+                        listeners.splice(i, 1);
+                    }
+                }
+            } else {
+                listeners = [];
+                groups = {};
+                callbacks = [];
+            }
+        }
+    };
+
+    // 初始化
+    lazyimg._init();
+
+
+    // 额外的绑定 jquery 插件
+    $.fn.lazyimg = function (config, type, callback) {
+
+        function noop() {
+
+        }
+
+        // 如果调用的是 detect 方法
+        if (config === 'detect') {
+            lazyimg.detect();
+            return;
+        }
+
+        // 如果调用的是 clear 方法
+        if (config === 'clear') {
+            lazyimg.clear(type);
+            return;
+        }
+
+        if (typeof config === 'object') {
+            lazyimg.config(config);
+        } else if (typeof config === 'function') {
+            type = 'img';
+            callback = config;
+        } else {
+            callback = type;
+            type = config;
+        }
+
+        // 如果不存在 || 类型不是 function
+        if (!callback || typeof callback !== 'function') {
+            callback = noop;
+        }
+
+        lazyimg.listen($(this), type, callback);
+    };
+
+    return lazyimg;
+}));
